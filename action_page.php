a730d243cb615185795a9dd08d9db431a74d5821
ReyhanGe <334480@student.mboutrecht.nl>
2022-01-12 00:28:41 +0100

    pagina's maken en bewerken
    maak formulier en toon record op andere pagina
    Ik heb pagina's maken, function, action en connect gemaakt en heb ze niet bewerkt

--- action_page.php
+++ action_page.php
@@ -2,50 +2,23 @@
 
 <?php
 
+include("./connect_db.php");
+
 $sql = "SELECT * FROM recensie";
 
 $result = mysqli_query($conn, $sql);
 
 
-
 $row = ""; 
     while ($record = mysqli_fetch_assoc($result)){
         $row .= "<tr>
-                     <td>{$record[`firstname`]}</td>
-                     <td>{$record[`lastname`]}</td>
-                     <td>{$record[`phone`]}</td>
-                     <td>{$record[`email`]}</td>
-                     <td>{$record[`subjectt`]}</td>
+                     <td>{$record['firstname']}</td>
+                     <td>{$record['lastname']}</td>
+                     <td>{$record['phone']}</td>
+                     <td>{$record['email']}</td>
+                     <td>{$record['subjectt']}</td>
                  </tr>";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-include("./connect_db.php");
-
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
-$tphone = $_POST["tphone"];
-$email = $_POST["email"];
-$subjectt = $_POST["subjectt"];
-
-
-$sql = "INSERT INTO recensie (`firstname`, `lastname`, `phone`, `email`, `subjectt`)
-VALUES ('$fname', '$lname', '$tphone', '$email', '$subjectt');";
-
-mysqli_query($conn, $sql);
 
 ?>
 
@@ -56,16 +29,53 @@ mysqli_query($conn, $sql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>RecentiePagina</title>
 </head>
 <body>
-    
+
+    <header>
+        <nav>
+            <ul>
+                <li> <a href="homepage.html"><img src="img/horizontal_on_white_by_logaster.png" alt="logo"></a></li> 
+                <li> <a href="">Categorie </a>  
+                    <ul> 
+                        <li> <a href="homepage.html"> TV </a> </li>
+                        <li> <a href="homepage.html"> Computer, Laptops, Monitoren </a> </li>
+                        <li> <a href="homepage.html"> Mobiel Telephone </a> </li>
+                    </ul>  
+                </li>       
+                <li> <a href="overons.html">Over Ons</a></li> 
+                <li> <a href="recentie.html">Recentiepagina </a> </li>
+                <li> <a href="contact.html ">Contact </a>  </li>
+                <li> <input type="text-search" placeholder="Search.."> </li>               
+            </ul>
+        </nav>
+    </header>
 
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+             <!-- de tabel met gegevens uit de databse komt hier -->
 
-
-
-
+                <table >
+                    <thead>
+                        <tr >
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>T-Nummer</th>
+                            <th>E-mail</th>
+                            <th>Bericht</th>
+                        </tr>     
+                    </thead>
+                    <tbody>                     
+                    <?php echo $row; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
